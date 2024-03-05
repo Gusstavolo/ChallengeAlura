9a98c89ca14075d87f53976efe63af03b41ac5f5
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-05 20:50:35 -0300

    update all

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import fotimg from './img/headerr.png';
+import back from './img/backg.png';
 
 
-function Dimg() {
+function Backg() {
   // Import result is the URL of your image
   return (
     <div className='dddDiv'>
-        <img className='headerimg' src={fotimg} alt="d" />
+        <img className='backg' src={back} alt="d" />
        
         
     </div>
@@ -17,4 +17,4 @@ function Dimg() {
   );
 }
 
-export default Dimg;
+export default Backg;
---
+++ src/components/imgdFoot.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import footer from '../components/img/footerimg.png';
+
+
+function Fimg() {
+  // Import result is the URL of your image
+  return (
+    <div className='dddDiv'>
+        <img className='Footimg' src={footer} alt="d" />
+
+    </div>
+        
+
+
+  );
+}
+
+export default Fimg;
