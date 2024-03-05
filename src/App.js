5d71f4b7131422217499058b58ae8fbb6f280eeb
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-05 11:04:13 -0300

    config vw vh, fullwindow

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div >
+    <div  className='main'>
       <header>
       </header>
       <section>
         <textarea typeof='text' itemID='criptografar-texarea' className='boxOne' placeholder="Digite seu texto..."></textarea>
         
+        <textarea typeof='text' itemID='descriptografar-texarea' className='boxTwo' placeholder="Digite seu texto..."></textarea>
+
 
       </section>
+      <footer></footer>
      
     </div>
   );
