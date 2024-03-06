4b5bf159a030055559b93e47dedbe9a5344b7c84
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-06 11:10:44 -0300

    fix fuction criptoconfig

--- src/components/criptoconfig.jsx
+++ src/components/criptoconfig.jsx
@@ -4,14 +4,16 @@ let textAreaDescripto = document.getElementById("descriptografar-texarea");
 
 
  export const clickButtonCripto = () => {
-            var txtValue = textAreaCripto.value;
-            txtValue = txtValue.replace(/e/g, "long").replace(/i/g, "pouuyn").replace(/a/g, "ober").replace(/u/g, "udumb");
-            textAreaDescripto.value = txtValue;
-
+        if (textAreaCripto && textAreaCripto.value) {
+                var txtValue = textAreaCripto.value;
+                txtValue = txtValue.replace(/e/g, "long").replace(/i/g, "pouuyn").replace(/a/g, "ober").replace(/u/g, "udumb");
+                textAreaDescripto.value = txtValue;
+            }
     }
 export const clickButtonDescripto = () => {
-        var txtValueDes = textAreaDescripto.value;
-        txtValueDes = txtValueDes.replace(/long/g, "e").replace(/ober/g, "a").replace(/udumb/g, "u").replace(/pouuyn/g, "i");
-        textAreaDescripto.value = txtValueDes;
-
+        if (textAreaDescripto && textAreaDescripto.value) {
+                var txtValueDes = textAreaDescripto.value;
+                txtValueDes = txtValueDes.replace(/long/g, "e").replace(/ober/g, "a").replace(/udumb/g, "u").replace(/pouuyn/g, "i");
+                textAreaDescripto.value = txtValueDes;
+            }
 }
